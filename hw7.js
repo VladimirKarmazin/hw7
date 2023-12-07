a3f55057c2d4817366e5a98b2a36f82f87c31c2c
VladimirKarmazin <128676499+VladimirKarmazin@users.noreply.github.com>
2023-12-07 16:36:50 +0200

    hw7

---
+++ hw7.js
@@ -0,0 +1,6 @@
+var number1 = parseFloat(prompt("Введіть перше число:"))
+var number2 = parseFloat(prompt("Введіть друге число:"))
+var number3 = parseFloat(prompt("Введіть третє число:"))
+
+var averege = (number1 + number2 + number3) / 3
+alert("Середнє арифметичне: " + averege)
